5dcfb1160b5ce16ef636ce0bd213da79b8c68dea
Ronald Twitty <btwitty187@gmail.com>
2026-08-28 17:56:32 -0500

    fix: hardcode GA measurement id

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: "G-5D7G52V0ZT",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
